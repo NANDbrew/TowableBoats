aace3003f2dfde41645e86120023f23a38fb1d9a
NANDbrew <zortman@gmail.com>
2023-12-23 19:21:30 -0600

    Adjust bollard positions/rotations

--- Plugin.cs
+++ Plugin.cs
@@ -12,7 +12,7 @@ namespace TowableBoats
     {
         public const string PLUGIN_ID = "com.nandbrew.towableboats";
         public const string PLUGIN_NAME = "Towable Boats";
-        public const string PLUGIN_VERSION = "0.0.1";
+        public const string PLUGIN_VERSION = "0.1.0";
 
         //--settings--
         internal static ConfigEntry<int> performanceMode;
@@ -26,7 +26,7 @@ namespace TowableBoats
             logSource = Logger;
             Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), PLUGIN_ID);
 
-            performanceMode = Config.Bind("Settings", "Performance mode", 1, new ConfigDescription("How many boats down the chain from the current boat should get full physics", new AcceptableValueRange<int>(0, 5), new ConfigurationManagerAttributes { Order = 1 }));
+            performanceMode = Config.Bind("Settings", "Performance mode", 1, new ConfigDescription("How many boats in the chain behind the current boat get full physics", new AcceptableValueRange<int>(0, 5), new ConfigurationManagerAttributes { Order = 1 }));
             multiParent = Config.Bind("Settings", "Team towing", false, new ConfigDescription("Allow a boat to BE TOWED BY multiple boats at once (buggy)", null, new ConfigurationManagerAttributes { IsAdvanced = true }));
             drag = Config.Bind("Settings", "Drag", false, new ConfigDescription("Allow boats to pull on the boat towing them (buggy)", null, new ConfigurationManagerAttributes { IsAdvanced = true }));
         }
